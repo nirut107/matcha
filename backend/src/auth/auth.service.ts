c95f1a465bcd700eaecd7befc9b5a9647403a529
nirut <atipanyo107@gmail.com>
2026-04-04 19:33:21 +0700

    set new token to 1d

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -187,7 +187,7 @@ export class AuthService {
 
     const accessToken = this.jwtService.sign(payload, {
       secret: process.env.JWT_ACCESS_SECRET,
-      expiresIn: '15m',
+      expiresIn: '1d',
     });
 
     const refreshToken = this.jwtService.sign(payload, {
