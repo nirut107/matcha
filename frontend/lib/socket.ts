9f69d651babc84431fa6df6d424fa575d29d47d9
gs313 <sirapat1charu@gmail.com>
2026-04-05 23:10:14 +0700

    socket.ts

--- frontend/lib/socket.ts
+++ frontend/lib/socket.ts
@@ -1,6 +1,14 @@
-import { io } from "socket.io-client";
+import { io, Socket } from "socket.io-client";
 
-export const socket = io("http://localhost:3001", {
-  withCredentials: true, // 🔥 VERY IMPORTANT (cookies)
-  transports: ["websocket"], // optional but cleaner
-});
+let socket: Socket | null = null;
+
+export const getSocket = (): Socket => {
+  if (!socket) {
+    socket = io("http://localhost:3001", {
+      withCredentials: true, // 🔥 VERY IMPORTANT (cookies for JWT)
+      transports: ["websocket"], // optional but faster
+    });
+  }
+
+  return socket;
+};
